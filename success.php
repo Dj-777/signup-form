d7cd3c3ac84bd8bbb214e2201d79c5be2de5d3aa
Dj-777 <dchampavat777@gmail.com>
2022-01-06 17:08:19 +0530

    success.php is deleted

--- success.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require 'header.php';
-?>
-<div class="alert alert-success" role="alert">
-		  Your Data Has Been Successfully Submited!
-		</div>
-		<?php
-			header( "refresh:5;url=registration.php" );
-		?>
