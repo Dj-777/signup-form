b665a40ea4786b4db51492af91d7fa4aa3ede9b9
Dj-777 <97013978+Dj-777@users.noreply.github.com>
2022-01-11 16:07:44 +0530

    Update registration.php

--- database/registration.php
+++ database/registration.php
@@ -145,8 +145,8 @@ if(isset($_REQUEST["submit"])){
 				$mail->isSMTP();                                      // Set mailer to use SMTP
 				$mail->Host = 'smtp.gmail.com;';  // Specify main and backup SMTP servers
 				$mail->SMTPAuth = true;                               // Enable SMTP authentication
-				$mail->Username = 'thedjgamer777@gmail.com';                 // SMTP username
-				$mail->Password = '05092001_dj';                           // SMTP password
+				$mail->Username = 'onlyforcompanywork@gmail.com';                 // SMTP username
+				$mail->Password = 'work@work';                           // SMTP password
 				$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 				$mail->Port = 587;                                    // TCP port to connect to
 
